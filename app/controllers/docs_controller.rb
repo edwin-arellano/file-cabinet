3502cf85a17222521de3d44263568d22a2d9cddc
Edwin Arellano <edwin.i.arellano@gmail.com>
2019-04-26 10:29:37 -0500

    Show view added

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,4 +1,6 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -31,6 +33,7 @@ class DocsController < ApplicationController
   private
 
     def find_doc
+      @doc = Doc.find(params[:id])
     end
 
     def doc_params
